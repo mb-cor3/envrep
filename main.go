80426698886807835edaee825cecce596ac8452d
Zuzuske <Vytautas.Vasiliauskas@outlook.com>
2023-03-17 13:31:44 +0200

    add support for standalone file

--- main.go
+++ main.go
@@ -7,61 +7,73 @@ import (
 	"regexp"
 )
 
-func main() {
-	// Get the directory to scan for YAML files.
-	dir := os.Getenv("DIR")
-	if dir == "" {
-		// If the environment variable is not set, use the k8s directory.
-		dir = "k8s"
+func processFile(path string, pattern *regexp.Regexp, info os.FileInfo, ext string) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
 	}
 
-	// Define the pattern to search for in the YAML files.
-	pattern := regexp.MustCompile(`\$\{(\w+)\}`)
-
-	// Scan the directory for YAML files.
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
+	newData := pattern.ReplaceAllStringFunc(string(data), func(match string) string {
+		key := match[2 : len(match)-1]
+		value := os.Getenv(key)
+		if value == "" {
+			return match
 		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		if filepath.Ext(path) != ".yaml" {
-			return nil
-		}
-
-		// Read the contents of the YAML file.
-		data, err := os.ReadFile(path)
-		if err != nil {
-			return err
-		}
-
-		// Replace the pattern in the YAML file with the environment variable value.
-		newData := pattern.ReplaceAllStringFunc(string(data), func(match string) string {
-			key := match[2 : len(match)-1]
-			value := os.Getenv(key)
-			if value == "" {
-				// If the environment variable is not set, use the original value.
-				return match
-			}
-			return value
-		})
-
-		// Write the new contents to the YAML file.
-		err = os.WriteFile(path, []byte(newData), info.Mode())
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Replaced values in %s\n", path)
-
-		return nil
+		return value
 	})
 
+	err = os.WriteFile(path, []byte(newData), info.Mode())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Replaced values in %s\n", path)
+
+	return nil
+}
+
+func main() {
+	path := os.Getenv("PATH")
+	if path == "" {
+		path = "k8s"
+	}
+
+	pattern := regexp.MustCompile(`\$\{(\w+)\}`)
+
+	info, err := os.Stat(path)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+
+	if info.IsDir() {
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			if filepath.Ext(path) != ".yaml" && filepath.Ext(path) != ".json" {
+				return nil
+			}
+
+			return processFile(path, pattern, info, filepath.Ext(path))
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else {
+		if filepath.Ext(path) == ".yaml" || filepath.Ext(path) == ".json" {
+			err = processFile(path, pattern, info, filepath.Ext(path))
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
 	}
 }
